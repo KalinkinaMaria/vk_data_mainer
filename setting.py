cc5e59045e291170aac8a83efded2aa44112f678
Мария Калинкина <mashyfycya@gmail.com>
2019-08-31 15:39:49 +0400

    add setting

---
+++ setting.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+#VK
+VK_SERVICE_KEY = "aa20b077aa20b077aa20b077cfaa4c2f06aaa20aa20b077f74abad731738061c3482fc3"
+VK_API_VERSION = '5.101'
+
+VK_DB_PATH = str(Path('db') / "statistics.db")
